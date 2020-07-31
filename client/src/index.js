c7199434acc0d6037a06ec8abfbb20dc08ce1870
Linus Löfgren <linus@ekensteenlofgren.se>
2020-07-31 15:05:42 +0200

    Use location.origin to set API_URL

--- client/src/index.js
+++ client/src/index.js
@@ -5,8 +5,10 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createClient, Provider } from 'urql'
 
+const API_URL = window.location.origin + '/graphql'
+console.log(API_URL)
 const client = createClient({
-  url: window.location.protocol + "//" + window.location.hostname + process.env.NODE_ENV === 'production' ? '' : ':4000' + '/graphql',
+  url: API_URL,
   fetchOptions: {
     credentials: 'include'
   }
